0aef7762a94768dd94d38cb170bc18bfd24f0d6d
Class01 <Class01@DESKTOP-V50GOLG>
2023-06-01 09:44:08 +0900

    3046

---
+++ baekq/src/q3046/Main.java
@@ -0,0 +1,19 @@
+package q3046;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+//		
+//		int r1 = sc.nextInt();
+//		int s = sc.nextInt();
+//		System.out.println(2*s-r1);
+		
+		System.out.println(-sc.nextInt()+2*sc.nextInt());
+		
+	}
+
+}
